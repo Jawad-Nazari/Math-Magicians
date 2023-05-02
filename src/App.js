da0571779a13bc5af88fa8325124cf6face65e13
Jawad <Jawad.Nazari@outlook.com>
2023-05-03 00:49:10 +0500

    Adding calculator components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Calculator from './components/calculator';
+
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Calculator />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
