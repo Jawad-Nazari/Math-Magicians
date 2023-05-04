635d80d06fa73e79a33c45976b685c9986c579b2
Jawad <Jawad.Nazari@outlook.com>
2023-05-04 23:54:49 +0500

    Adding new component to fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Calculator from './components/calculator';
+import Quote from './components/quote';
 import './App.css';
 
 const App = () => (
   <div className="App">
     <header className="App-header">
       <Calculator />
+      <Quote />
     </header>
   </div>
 );
